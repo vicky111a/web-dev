06973b01166d476b445c9bdb20640ea8c334e8b4
vicky111a <vivekanand.amand@gmail.com>
2022-12-15 23:45:48 +0530

    booking code update in py

--- codep.py
+++ codep.py
@@ -1,3 +1,19 @@
-a=5
-a=99
-print(a)
+class flight():
+    def __init__(self,count):
+        self.tseats=count
+        self.passengers=[]
+    def add(self,name):
+        self.passengers.append(name)
+    def available(self):
+        if (self.tseats-len(self.passengers))>0:
+            return True
+        return False
+a=flight(4)
+n=list(input("enter passengers list").split())
+c=True
+for i in n:
+    if a.available():
+        a.add(i)
+        print(f"ticket for {i} is booked")
+    else:
+        print(f"cant book ticket for {i}")
